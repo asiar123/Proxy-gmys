7137022b9f0f3b5d37766fdfb96850bf3923e58c
asiar123 <michael_2504@hotmail.com>
2024-10-17 13:50:41 -0500

    first commit

---
+++ proxy-server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const qs = require('qs'); // Asegúrate de haber importado qs
+const https = require('https');
+
+const app = express();
+
+// Habilita CORS para permitir solicitudes desde cualquier origen
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Para procesar datos urlencoded
+
+// Crear un agente HTTPS que use TLS v1.2 y que ignore la verificación de certificados
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+  secureProtocol: 'TLSv1_2_method',
+});
+
+// Ruta para manejar el login
+app.post('/login', async (req, res) => {
+  try {
+    const { usuario, passwd } = req.body;
+    console.log('Datos enviados al backend:', usuario, passwd);
+
+    // Envía la solicitud al web service utilizando el agente con TLS 1.2
+    const response = await axios.post('https://ws.gmys.com.co/login', qs.stringify({
+      usuario: usuario,
+      passwd: passwd
+    }), {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      httpsAgent: agent
+    });
+
+    console.log('Respuesta del backend:', response.data);
+
+    // Devuelve la respuesta del web service al frontend
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error en la solicitud:', error);
+    res.status(500).json({ error: 'Error al conectarse con el backend' });
+  }
+});
+
+// Configuración del puerto
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor proxy escuchando en el puerto ${port}`);
+});
