9b86131dac2a6c54ce40ff0058629cfeb6791fa9
asiar123 <michael_2504@hotmail.com>
2024-12-12 19:15:43 -0500

    Agregar middleware para permitir siempre cors en todas las solicitudes

--- proxy-server.js
+++ proxy-server.js
@@ -35,6 +35,20 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
 }));
 
+// Middleware adicional para asegurar encabezados y manejar preflight requests
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // Responder rápidamente a solicitudes OPTIONS
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('trust proxy', 1); // Enable trust for proxies
